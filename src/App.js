b656ac8e4c8b7fbdf84da870802d2f5e4f1a5758
kalhollywood <kal.hollywood@gmail.com>
2022-08-21 14:36:04 +0100

    home added but routes not working to display sidebar, have to remove index and change to path for it to return

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
+import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route index element={<Home />} />
         <Route path="/" element={<Layout />} />
 
+
       </Routes>
-    </div>
+    </div >
   );
 }
 
