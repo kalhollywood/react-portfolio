4f5f1858c6e3a72b29035cb052f32191f2346dcd
kalhollywood <kal.hollywood@gmail.com>
2022-08-21 11:41:26 +0100

    setup routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Routes>
+        <Route path="/" element={<Layout />} />
+
+      </Routes>
     </div>
   );
 }
